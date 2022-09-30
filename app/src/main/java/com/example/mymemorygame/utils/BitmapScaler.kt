2b23b11e0393985ce776a64f8130ec92def6e26c
swwlad03 <sw.wlad03@gmail.com>
2022-09-30 13:51:09 +0200

    MyMemoryGame

---
+++ app/src/main/java/com/example/mymemorygame/utils/BitmapScaler.kt
@@ -0,0 +1,16 @@
+package com.example.mymemorygame.utils
+
+import android.graphics.Bitmap
+
+object BitmapScaler {
+
+    fun scaleToFitWidth(b: Bitmap, width: Int): Bitmap {
+        val factor: Float = width / b.width.toFloat()
+        return Bitmap.createScaledBitmap(b, width, (b.height * factor).toInt(), true)
+    }
+
+    fun scaleToFitHeight(b: Bitmap, height: Int): Bitmap {
+        val factor: Float = height / b.height.toFloat()
+        return Bitmap.createScaledBitmap(b, (b.width * factor).toInt(), height, true)
+    }
+}
---
+++ app/src/main/java/com/example/mymemorygame/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.example.mymemorygame.utils
+
+import com.example.mymemorygame.R
+
+const val EXTRA_GAME_NAME = "EXTRA_GAME_NAME"
+const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_bin,
+    R.drawable.ic_bolt,
+    R.drawable.ic_car,
+    R.drawable.ic_dumbbell,
+    R.drawable.ic_face,
+    R.drawable.ic_fastfood,
+    R.drawable.ic_headphones,
+    R.drawable.ic_heart,
+    R.drawable.ic_leaf,
+    R.drawable.ic_plane,
+    R.drawable.ic_puzzle,
+    R.drawable.ic_train
+)
